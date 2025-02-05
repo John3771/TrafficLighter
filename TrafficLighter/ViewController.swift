bc438740d463f38a3b8f1061e3b0664791ffa67f
Gleb <w>
2025-02-05 19:59:41 +0300

    Initial Commit

---
+++ TrafficLighter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLighter
+//
+//  Created by Глеб Соколов on 5.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
