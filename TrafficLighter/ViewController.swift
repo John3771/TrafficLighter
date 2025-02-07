585b95a84d233ed590b866c5ed27345373b99a1a
Gleb <w>
2025-02-07 03:19:24 +0300

    123

--- TrafficLighter/ViewController.swift
+++ TrafficLighter/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var TopLightView: UIView!
+    @IBOutlet weak var MidLightView: UIView!
+    @IBOutlet weak var LowLightView: UIView!
+    @IBOutlet weak var ButtonLightChanger: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        TopLightView.layer.cornerRadius = 64
+        MidLightView.layer.cornerRadius = 64
+        LowLightView.layer.cornerRadius = 64
+        ButtonLightChanger.layer.cornerRadius = 8
+        
     }
 
-
+    @IBAction func stateChangerButton(_ sender: UIButton) {
+        
+    }
 }
 
